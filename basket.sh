bc54422a02866ce1a32d744a628d3b6ea8218841
XAliX666 <95588662+XAliX666@users.noreply.github.com>
2023-01-12 16:08:39 +0300

    Add files via upload

--- basket.sh
+++ basket.sh
@@ -2,8 +2,11 @@
 
 #development no finish
 
-#check dir TRESH files which chill two days.
+#check dir TRESH files which chill long  two days.
 find /home/ali/TRESH -type f -mtime +2 -delete
+
+#Creat directory because have not
+mkdir -p /home/ali/TRESH
 
 #test arguments
 if [ $# -ne 1 ]
@@ -12,14 +15,15 @@ then
     exit 1
 fi
 
+#Test have or have not
 if [ ! -e $1 ]
 then
     echo "$1 no found is file or directory."
     exit 1
 fi
 
-#
-if [lzma $1]
+#Compressions file in lzma
+if (lzma $1)
 then 
     mv $1.lzma /home/ali/TRESH
 fi
