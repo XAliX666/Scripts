e4ecade75543c3b311dd93a9e104d5dc1c3762cd
Gamzaev <mamedzade@MacBook-Air-Pobeda.local>
2023-01-17 00:46:12 +0300

    Add enc descrypt

---
+++ file_descrypt_keys.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# $1 - file.txt.enc $2 - private.key
+#Test file $1
+if [ ! $1 == *.txt.enc ]
+then
+    echo "First arguments don't is this file.txt.enc"
+    exit 1
+fi
+#Test need two arguments
+if [ $# -ne 2 ]
+then
+    echo "Arguments need two"
+    exit 1
+fi
+#Test key
+if [[ !  $2 == *.key  ]]
+then
+    echo "Second arguments not is key"
+    exit 1
+fi
+
+openssl rsautl -decrypt -inkey $2 -in $1  -out "$1"decrypt.new
