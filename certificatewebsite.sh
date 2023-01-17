e5194e44f2f46ec6c4e2a5f1b36c4522a7df174d
XAliX666 <95588662+XAliX666@users.noreply.github.com>
2023-01-17 21:01:59 +0300

    Add files via upload

--- certificatewebsite.sh
+++ certificatewebsite.sh
@@ -4,4 +4,6 @@ read -r -p "IP host: " iphost
 read -r -p "Name host: " namehost
 read -r  -s -p  "Enter password for sudo: " passwd
 
-$passwd | sudo -S  echo $iphost  $namehost>>/etc/hosts
+echo $passwd | sudo -S chmod g+w /etc/hosts
+echo $passwd | sudo -S  echo "$iphost  $namehost" >> /etc/hosts 
+echo $passwd | sudo -S chmod g-w /etc/hosts
